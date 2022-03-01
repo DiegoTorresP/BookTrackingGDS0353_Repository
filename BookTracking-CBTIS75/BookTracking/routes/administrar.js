6dde5916ea7d3b7e5177781f70c29a976848245f
alejandro026 <alejandro02262@gmail.com>
2022-02-28 22:35:28 -0600

    Menu y ruta de alumno
    Se muestra el menu del alumno

--- BookTracking-CBTIS75/BookTracking/routes/administrar.js
+++ BookTracking-CBTIS75/BookTracking/routes/administrar.js
@@ -12,18 +12,22 @@ router.get("/", function (req, res, next) {
 router.get("/libros", function (req, res, next) {
   res.render("admin_buscar");
 });
+
 router.get("/solicitudes", function (req, res, next) {
   res.render("solicitudes");
 });
+
 router.get("/registrar-usuario", function (req, res, next) {
   res.render("registrar_user");
 });
 
 router.get("/vista-libro", function (req, res, next){
   res.render("admin_vista_libro");
-})
+});
 
-
+router.get("/menu-libro", function (req, res, next){
+  res.render("libros");
+});
 
 
 module.exports = router;
--- BookTracking-CBTIS75/BookTracking/routes/index.js
+++ BookTracking-CBTIS75/BookTracking/routes/index.js
@@ -5,5 +5,4 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.redirect('/administrar');
 });
-
 module.exports = router;
