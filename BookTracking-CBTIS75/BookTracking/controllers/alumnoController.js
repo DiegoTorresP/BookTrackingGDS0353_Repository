350607f0fe6fb44a849994838f03c6db7b99ee0c
alejandro026 <alejandro02262@gmail.com>
2022-03-12 16:21:39 -0600

    Editar campos
    Se agrega la parte de editar un alumno en especifico

--- BookTracking-CBTIS75/BookTracking/controllers/alumnoController.js
+++ BookTracking-CBTIS75/BookTracking/controllers/alumnoController.js
@@ -59,7 +59,7 @@ alumnoController.crear = (req, res) => {
 };
 
 //Editar
-//Mostar y editar un alumno
+//Mostar el alumno a editar
 alumnoController.editar= (req, res)=>{
     const id= req.params.id;
     Alumno.find({"_id":id}).exec((err, Alumno) => {
@@ -73,6 +73,53 @@ alumnoController.editar= (req, res)=>{
             Alumno: Alumno
         });
     });
+}
+
+//Editar el alumno mostrado
+alumnoController.editar1=(req, res)=>{
+        const _id= req.body.numControl;
+        const Apellido_Paterno= req.body.apPaterno;
+        const Apellido_Materno= req.body.apMaterno;
+        const Nombre_s= req.body.nombre;
+        const Genero= req.body.sexo;
+        const CURP= req.body.curp;
+        const Carrera_Tecnica= req.body.especialidad;
+        const Turno= req.body.turno;
+        const Clave_lada= req.body.lada;
+        const Telefono_de_contacto_fijo_1= req.body.telefonoFijo1;
+        const Telefono_de_contacto_fijo_2= req.body.telefonoFijo2;
+        const Telefono_movil_celular= req.body.telefonoMovil;
+        const Correo_Electronico_1= req.body.correo1;
+        const Correo_Electronico_2= req.body.correo2;
+        const Grupo= req.body.grupo;
+        const Generacion= req.body.generacion;
+
+        Alumno.updateOne({"_id":_id},{$set:{
+            "_id":_id,
+            "Apellido_Paterno":Apellido_Paterno,
+            "Apellido_Materno":Apellido_Materno,
+            "Nombre_s":Nombre_s,
+            "Genero":Genero,
+            "CURP":CURP,
+            "Carrera_Tecnica":Carrera_Tecnica,
+            "Turno":Turno,
+            "Clave_lada":Clave_lada,
+            "Telefono_de_contacto_fijo_1":Telefono_de_contacto_fijo_1,
+            "Telefono_de_contacto_fijo_2":Telefono_de_contacto_fijo_2,
+            "Telefono_movil_celular":Telefono_movil_celular,
+            "Correo_Electronico_1":Correo_Electronico_1,
+            "Correo_Electronico_2":Correo_Electronico_2,
+            "Grupo":Grupo,
+            "Generacion":Generacion
+        }}).exec((err, Alumno) => {
+            if (err) {
+                console.log('Error al actalizar el alumno:', err);
+                return;
+            }
+            console.log("The INDEX");
+            console.log(Alumno)
+            res.redirect('/administrar/lista-usuario');
+        });
 }
 
 //Eliminar
